dd8ffacb6194f88cf25a2a294be5c1cf61565f70
Aleksandr <alex05061981@yandex.ru>
2022-10-30 18:18:21 +0300

    Добавили задача2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимет на вход три числа и выдает максимальное из этих чисел
+{
+   int num1, num2, num3;
+   Console.WriteLine("Введите три целочисленных числа: ");
+   num1 = Convert.ToInt32(Console.ReadLine());
+   num2 = Convert.ToInt32(Console.ReadLine());
+   num3 = Convert.ToInt32(Console.ReadLine());
+   if (num1>num2)
+   {
+      if (num1>num3)
+      {
+         Console.WriteLine("{0} наибольшее число", num1);
+      }
+      else
+      {
+         Console.WriteLine("{0} наибольшее число", num3);
+      }
+    }
+    else
+    {
+      if (num2>num3)
+      {    
+        Console.WriteLine("{0} наибольшее число", num2);
+      }
+      else
+      {
+        Console.WriteLine("{0} наибольшее число", num3);
+      }
+    }
+    Console.ReadLine();
+}
