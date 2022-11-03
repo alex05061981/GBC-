fde1f23b804823db06445188c5690ead92e4cde0
Aleksandr <alex05061981@yandex.ru>
2022-11-03 10:30:37 +0300

    Добавили задача29

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] arr = {2, 4, 6, 8, 10, 12, 14, 16};
+void PtintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]},");
+
+    }
+    Console.WriteLine();
+
+}
+PtintArray(arr);
