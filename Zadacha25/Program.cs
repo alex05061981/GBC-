d0d9e6be2361ecdc6a261893a494002944c5e6ce
Aleksandr <alex05061981@yandex.ru>
2022-11-03 10:29:59 +0300

    Добавили задача25

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
+
+for (int i = 1; i < b; i++)
+{
+step = step * a;
+}
+Console.WriteLine("A в степени B равно: " + step);
